06ebd09653d9ba2694606d52bac5900e0a8ebcfd
silveiralsv <silveira.lsv@gmail.com>
2022-02-21 11:06:24 -0300

    cors

--- services/api.ts
+++ services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const weatherApi = axios.create({
-  baseURL: 'http://api.openweathermap.org/data/2.5',
+  baseURL: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5',
 });
 
 export const geoApi = axios.create({
-  baseURL: 'http://api.openweathermap.org/geo/1.0',
+  baseURL: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/geo/1.0',
 });
