9fa3685c850d941871513c32639752ac979c5af0
silveiralsv <silveira.lsv@gmail.com>
2022-02-21 10:25:35 -0300

    Removed unused

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ReminderProvider } from '../hooks/reminder';
-import { MapProvider } from '../hooks/map';
 import { ModalProvider } from '../hooks/modal';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MapProvider>
-      <ReminderProvider>
-        <ModalProvider>
-          <Component {...pageProps} />
-        </ModalProvider>
-      </ReminderProvider>
-    </MapProvider>
+    <ReminderProvider>
+      <ModalProvider>
+        <Component {...pageProps} />
+      </ModalProvider>
+    </ReminderProvider>
   );
 }
 
